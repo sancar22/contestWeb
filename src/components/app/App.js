1f89be10658f542591c4350dcef5416acc463ecf
unknown <santiagoyacaman@uninorte.edu.co>
2020-01-25 12:19:34 -0500

    Active_cases_red_close_cross

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -14,9 +14,9 @@ import firebase from "../../routes/Config";
 import _ from "lodash";
 import { useDispatch } from "react-redux";
 import {
-    selectOnlineGuards,
-    selectAll,
-    selectActiveCases,
+  selectOnlineGuards,
+  selectAll,
+  selectActiveCases
 } from "../../actions/index";
 import ClipLoader from "react-spinners/ClipLoader";
 import { css } from "@emotion/core";
@@ -25,87 +25,87 @@ import "react-toastify/dist/ReactToastify.css";
 import { messaging } from "../../init-fcm";
 
 toast.configure({
-    autoClose: 8000,
-    draggable: false,
+  autoClose: 8000,
+  draggable: false
 });
 
 function App() {
-    const [firebaseInitialized, setFirebaseInitialized] = useState(false);
-    const dispatch = useDispatch();
-    const override = css`
-        display: block;
-        margin: 0 auto;
-        border-color: red;
-        margin-top: 40vh;
-    `;
+  const [firebaseInitialized, setFirebaseInitialized] = useState(false);
+  const dispatch = useDispatch();
+  const override = css`
+    display: block;
+    margin: 0 auto;
+    border-color: red;
+    margin-top: 40vh;
+  `;
 
-    function getDataOnlineUsers() {
-        // Para obtener información de todos los usuarios online
-        return app
-            .database()
-            .ref("/Users")
-            .orderByChild("online")
-            .equalTo(true)
-            .on("value", snapshot => {
-                const firebaseData = _.toArray(snapshot.val());
-                dispatch(selectOnlineGuards(firebaseData)); // Se hace un dispatch a la store para guardarlo en el estado global
-            });
-    }
+  function getDataOnlineUsers() {
+    // Para obtener información de todos los usuarios online
+    return app
+      .database()
+      .ref("/Users")
+      .orderByChild("online")
+      .equalTo(true)
+      .on("value", snapshot => {
+        const firebaseData = _.toArray(snapshot.val());
+        dispatch(selectOnlineGuards(firebaseData)); // Se hace un dispatch a la store para guardarlo en el estado global
+      });
+  }
 
-    function getDataAllUsers() {
-        return app
-            .database()
-            .ref("/Users")
-            .orderByChild("acceptRatio")
-            .on("value", snapshot => {
-                const firebaseData = _.toArray(snapshot.val());
-                dispatch(selectAll(firebaseData));
-            });
-    }
+  function getDataAllUsers() {
+    return app
+      .database()
+      .ref("/Users")
+      .orderByChild("acceptRatio")
+      .on("value", snapshot => {
+        const firebaseData = _.toArray(snapshot.val());
+        dispatch(selectAll(firebaseData));
+      });
+  }
 
-    function getDataActiveCases() {
-        return app
-            .database()
-            .ref("/Casos")
-            .orderByChild("active")
-            .equalTo(true)
-            .on("value", snapshot => {
-                const firebaseData = _.toArray(snapshot.val());
-                dispatch(selectActiveCases(firebaseData));
-            });
-    }
+  function getDataActiveCases() {
+    return app
+      .database()
+      .ref("/Casos")
+      .orderByChild("closed")
+      .equalTo(false)
+      .on("value", snapshot => {
+        const firebaseData = _.toArray(snapshot.val());
+        dispatch(selectActiveCases(firebaseData));
+      });
+  }
 
-    useEffect(() => {
-        // Se va a ejecutar una vez
-        firebase.isInitialized().then(val => {
-            setFirebaseInitialized(val);
-        });
-        firebase.resetSelected(); // Para al refrescar la página deseleccionar todos los marcadores
-        getDataOnlineUsers();
-        getDataAllUsers();
-        getDataActiveCases();
-    }, []);
+  useEffect(() => {
+    // Se va a ejecutar una vez
+    firebase.isInitialized().then(val => {
+      setFirebaseInitialized(val);
+    });
+    firebase.resetSelected(); // Para al refrescar la página deseleccionar todos los marcadores
+    getDataOnlineUsers();
+    getDataAllUsers();
+    getDataActiveCases();
+  }, []);
 
-    return firebaseInitialized !== false ? (
-        <Router>
-            <div>
-                <Switch>
-                    <Route exact path={ROUTES.SIGN_UP} component={SignUp} />
-                    <Route exact path={ROUTES.HOME} component={HomePage} />
-                    <Route exact path={ROUTES.SIGN_IN} component={SignIn} />
-                    <Route exact path={ROUTES.STATS} component={Stats} />
-                </Switch>
-            </div>
-        </Router>
-    ) : (
-        <ClipLoader
-            css={override}
-            sizeUnit={"px"}
-            size={150}
-            color={"#123abc"}
-            loading={!firebaseInitialized}
-        />
-    );
+  return firebaseInitialized !== false ? (
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path={ROUTES.SIGN_UP} component={SignUp} />
+          <Route exact path={ROUTES.HOME} component={HomePage} />
+          <Route exact path={ROUTES.SIGN_IN} component={SignIn} />
+          <Route exact path={ROUTES.STATS} component={Stats} />
+        </Switch>
+      </div>
+    </Router>
+  ) : (
+    <ClipLoader
+      css={override}
+      sizeUnit={"px"}
+      size={150}
+      color={"#123abc"}
+      loading={!firebaseInitialized}
+    />
+  );
 }
 
 export default App;
--- src/components/caseForm/ActiveCaseBlock.js
+++ src/components/caseForm/ActiveCaseBlock.js
@@ -1,91 +1,113 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./CaseForm.css";
 
 function ActiveCaseBlock({
-    handleButtonClickCam,
-    handleButtonClickExt,
-    caso,
-    ...props
+  handleButtonClickCam,
+  handleButtonClickExt,
+  handleRedButton,
+  caso,
+  ...props
 }) {
-    return (
-        <div className="activeCaseLoop">
-            <div style={{ textAlign: "center" }} className="texti">
-                {caso.inicioFecha}
-            </div>
-            <br />
-            <div className="texti">
-                Nombre: {caso.nombre + " " + caso.apellido}
-            </div>
-            <div className="texti">Lugar: {caso.lugar}</div>
+  const [timer, setTimer] = useState(null);
 
-            <div className="texti">Código: {caso.codigo}</div>
-            <div className="texti">Categoría: {caso.categoria}</div>
+  useEffect(() => {
+    let b = caso.date;
+    console.log("Hello");
+    const timer = setInterval(() => {
+      if (!caso.active) {
+        clearInterval(timer);
+        setTimer(Math.round((caso.calcFin - b) / 1000)); // it stays static when case is closed
+      } else {
+        let a = new Date();
 
-            <br />
-            <div className="requests">
-                <div style={{ textAlign: "center" }} className="texti">
-                    Apoyos solicitados
-                </div>
-                <div className="requestsContainer">
-                    <div className="objects">
-                        <div
-                            className="texti"
-                            style={{ width: "100%", textAlign: "center" }}
-                        >
-                            Objetos
-                        </div>
-                        {caso.camilla && (
-                            <button
-                                id="camilla"
-                                onClick={() => handleButtonClickCam(caso)}
-                                disabled={caso.camillaCheck && true}
-                                style={{
-                                    backgroundColor:
-                                        caso.camillaCheck && "green",
-                                }}
-                                className="buttonObj"
-                            >
-                                {caso.camillaCheck ? (
-                                    <strike className="texti">Camilla</strike>
-                                ) : (
-                                    <div className="texti">Camilla</div>
-                                )}
-                            </button>
-                        )}
-                        {caso.extintor && (
-                            <button
-                                id="extintor"
-                                onClick={() => handleButtonClickExt(caso)}
-                                disabled={caso.extintorCheck && true}
-                                style={{
-                                    backgroundColor:
-                                        caso.extintorCheck && "green",
-                                }}
-                                className="buttonObj"
-                            >
-                                {caso.extintorCheck ? (
-                                    <strike className="texti">Extintor</strike>
-                                ) : (
-                                    <div className="texti">Extintor</div>
-                                )}
-                            </button>
-                        )}
-                        {caso.apoyo && (
-                            <button className="buttonObj">Apoyo</button>
-                        )}
-                    </div>
-                    <div className="listApoyo">
-                        <div
-                            className="texti"
-                            style={{ width: "100%", textAlign: "center" }}
-                        >
-                            Brigadistas
-                        </div>
-                    </div>
-                </div>
-            </div>
+        setTimer(Math.round((a - b) / 1000));
+      }
+    }, 1000);
+    return () => {
+      clearInterval(timer);
+    };
+  }, [caso.active]);
+  return (
+    <div className="activeCaseLoop">
+      <a
+        onClick={() => handleRedButton(caso)}
+        className="close-icon"
+        style={{ display: caso.active && "none" }}
+      ></a>
+      <div style={{ textAlign: "center" }} className="texti">
+        {caso.inicioFecha}
+      </div>
+      <br />
+      <div className="texti">Nombre: {caso.nombre + " " + caso.apellido}</div>
+      <div className="texti">Lugar: {caso.lugar}</div>
+
+      <div className="texti">Código: {caso.codigo}</div>
+      <div className="texti">Categoría: {caso.categoria}</div>
+      <div className="texti">
+        Tiempo activo: {new Date(timer * 1000).toISOString().substr(11, 8)}
+      </div>
+
+      <br />
+      <div className="requests">
+        <div style={{ textAlign: "center" }} className="texti">
+          Apoyos solicitados
         </div>
-    );
+        <div className="requestsContainer">
+          <div className="objects">
+            <div
+              className="texti"
+              style={{ width: "100%", textAlign: "center" }}
+            >
+              Objetos
+            </div>
+            {caso.camilla && (
+              <button
+                id="camilla"
+                onClick={() => handleButtonClickCam(caso)}
+                disabled={caso.camillaCheck && true}
+                style={{
+                  backgroundColor: caso.camillaCheck && "green"
+                }}
+                className="buttonObj"
+              >
+                {caso.camillaCheck ? (
+                  <strike className="texti">Camilla</strike>
+                ) : (
+                  <div className="texti">Camilla</div>
+                )}
+              </button>
+            )}
+            {caso.extintor && (
+              <button
+                id="extintor"
+                onClick={() => handleButtonClickExt(caso)}
+                disabled={caso.extintorCheck && true}
+                style={{
+                  backgroundColor: caso.extintorCheck && "green"
+                }}
+                className="buttonObj"
+              >
+                {caso.extintorCheck ? (
+                  <strike className="texti">Extintor</strike>
+                ) : (
+                  <div className="texti">Extintor</div>
+                )}
+              </button>
+            )}
+            {caso.apoyo && <button className="buttonObj">Apoyo</button>}
+          </div>
+          <div className="listApoyo">
+            <div
+              className="texti"
+              style={{ width: "100%", textAlign: "center" }}
+            >
+              Brigadistas
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ActiveCaseBlock;
